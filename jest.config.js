4b2ad16f539a29966159a5ab45d4de6478587ab1
jasonliao <jasonliao94@gmail.com>
2018-10-04 15:02:15 +0800

    feat: add babel config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,24 @@
 module.exports = {
   verbose: true,
   moduleFileExtensions: ['tsx', 'ts', 'js', 'jsx'],
-  setupTestFrameworkScriptFile: '<rootDir>/src/setupTests.ts',
+  setupTestFrameworkScriptFile: '<rootDir>/test/setupTests.js',
   testMatch: [
     '**/__tests__/**/*.ts?(x)',
     '**/?(*.)+(spec|test).ts?(x)'
   ],
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest'
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest'
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": '<rootDir>/__mocks__/styleMock.ts'
+    "\\.(css|less|sass|scss)$": '<rootDir>/test/__mocks__/styleMock.js'
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [151001]
+      }
+    }
   }
 }
--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: [path.resolve(__dirname, '../src/index.tsx')],
+  entry: [path.resolve(__dirname, '../src/index.js')],
   output: {
     path: path.resolve(__dirname, '../build/lib/'),
     filename: 'index.js',
